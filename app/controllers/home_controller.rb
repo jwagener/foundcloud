6e60069b0ec66c0e27ebccf22e65d1bc1d14a90a
Johannes Wagener <johannes@soundcloud.com>
2010-10-03 17:44:04 +0200

    fixing the routing. a bit of layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @tag = Tag.find_by_permalink(params[:permalink])
     @tags = Tag.all(:order => 'position ASC')
+    p params[:permalink]
+    p @tag
   end
 
   def show
     @tag = Tag.find_by_permalink(params[:permalink])
-
-    raise ActiveRecord::RecordNotFound if @tag.nil?  
-    
+    raise ActiveRecord::RecordNotFound if @tag.nil?
     respond_to do |format|
       format.html
       format.xml  { render :xml =>  @tag.to_xml  }
