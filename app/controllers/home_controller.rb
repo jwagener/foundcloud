00772975642a7aa94e2fafcaab7c298188ecc7ae
Johannes Wagener <johannes@soundcloud.com>
2010-10-03 14:50:54 +0200

    restructuring a bit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,13 @@ class HomeController < ApplicationController
 
   def show
     @tag = Tag.find_by_permalink(params[:permalink])
-    raise ActiveRecord::RecordNotFound if @tag.nil?  
-  end
 
+    raise ActiveRecord::RecordNotFound if @tag.nil?  
+    
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml =>  @tag.to_xml  }
+      format.json { render :json => @tag.to_json }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,56 +6,12 @@ ActionController::Routing::Routes.draw do |map|
 
   map.namespace :admin do |admin|
     admin.resources :tags, :active_scaffold  => :tag
-    
-#    admin.connect '/admin/do_add_user', :controller=>"admin/tags", :action=>"do_add_user_link"
-#    admin.connect '/admin/tags/:id/add_user',    :controller=>"admin/tags", :action=>"add_user_link"
-
-#admin.connect '/admin/tags/:id/do_add_user', :controller=>"admin/tags", :action=>"do_add_user_link"
-#admin.connect '/admin/tags/:id/add_user',    :controller=>"admin/tags", :action=>"add_user_link"
-
-admin.connect '/tags/:id/do_add_user', :controller=>"tags", :action=>"do_add_user_link"
-admin.connect '/tags/:id/add_user',    :controller=>"tags", :action=>"add_user_link"
-
+    admin.connect '/tags/:id/do_add_user', :controller=>"tags", :action=>"do_add_user_link"
+    admin.connect '/tags/:id/add_user',    :controller=>"tags", :action=>"add_user_link"
   end
-  
 
-#  map.resources :tags, :active_scaffold  => :tag
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "home"
-  map.tag  '/:permalink', :controller => :home, :action => :show
+  map.root :controller => :home
+  map.tag  '/:permalink.:format', :controller => :home, :action => :show
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
